23ba62de12966aa082487e3243fe5d36643bfced
franciscosacchetto <franciscosacchettoandrade@gmail.com>
2025-06-25 14:43:42 -0300

    Ex1

---
+++ Exercicio02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int idade;
+    string nome;
+    cout <<"Boas-Vindas esse é o codigo" <<endl;
+    cout <<"Digite sua idade" << endl;
+    cin >>  idade;
+    cout << " Sua idade é  " << idade << ", certo?" << endl;
+    cout <<"Muito Bem! Agora me fale seu nome " <<endl;
+    cin >> nome;
+    cout << "Seu nome é "<< nome << ",certo?" << endl;
+}
